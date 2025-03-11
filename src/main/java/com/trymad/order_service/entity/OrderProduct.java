3d35c60ff9bd0a2b63d33a3fbd7a90787edd8e8c
trymad <trymad@gmail.com>
2025-03-11 15:04:11 +0300

    delete unused imports

--- src/main/java/com/trymad/order_service/entity/OrderProduct.java
+++ src/main/java/com/trymad/order_service/entity/OrderProduct.java
@@ -1,6 +1,5 @@
 package com.trymad.order_service.entity;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
